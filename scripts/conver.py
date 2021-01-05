d2850941cf126e377e528039d33142c73418789e
kaito-toda <s19c1082ar@s.chibakoudai.jp>
2021-01-05 12:34:50 +0000

    LatestChanges

---
+++ scripts/conver.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Int32
+
+val = 1 
+val_t = 1000
+n = 1
+c = 0
+def cb(message):
+    global n
+    n = message.data
+
+if __name__ == '__main__':
+    rospy.init_node('conver')
+    sub = rospy.Subscriber('count_up',Int32,cb)
+    rate = rospy.Rate(5)
+    while not rospy.is_shutdown():
+        if c > 100:
+            rate = rospy.Rate(10)
+            if c > 200:
+                rate = rospy.Rate(20)
+        if val < val_t:
+            print(val ,'+', n , end='=')
+            val = val + n
+            print(val,end='   ')
+            c += 1
+        elif val > val_t:
+            print(val ,'-', n , end='=')
+            val = val - n
+            print(val,end='   ')
+            c += 1
+        elif val == val_t:
+            c += 1
+            print(c,"回目で成功")
+            break
+        print(c ,"回目")
+        rate.sleep()
---
+++ scripts/count.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import rospy
+import random
+from std_msgs.msg import Int32
+
+rospy.init_node('count')
+pub = rospy.Publisher('count_up',Int32,queue_size=1)
+rate = rospy.Rate(20)
+n = 1
+while not rospy.is_shutdown():
+    n = random.randint(0,100)
+    pub.publish(n)
+    rate.sleep()
